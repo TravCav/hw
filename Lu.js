3311946775a7942be37e31616c17297a3d4ee340
TravCav <xurrux@gmail.com>
2015-04-30 22:35:11 -0400

    work happens

--- Lu.js
+++ Lu.js
@@ -1,10 +1,15 @@
 var Entity = require("./Entity.js");
+var Work = require("./Work.js");
 
 (function () {
 	console.log('begin');
 
-	var user = new Entity();
-	console.log(user.skills);
+	var user1 = new Entity();
+	var user2 = new Entity();
+	user1.name = "pep1";
+	user2.name = "pe2";
+
+	Work.Do(user1,user2);		
 
 	console.log('done');
 })();
