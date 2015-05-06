07951d4b197341566381ad1eaa4e3223797846a8
TravCav <xurrux@gmail.com>
2015-05-06 11:26:46 -0400

    work stuff

--- Work.js
+++ Work.js
@@ -5,18 +5,12 @@ var Do = function (entity1, entity2) {
     this.entity2 = entity2;
 
     console.log(this.entity1.name + " vs " + this.entity2.name);
-    CheckProgress();
+    this.entity2.energy -= this.entity1.skills[0].energyDrain;
 
-};
-
-function CheckProgress() {
     var loser = 'na';
-
     if (this.entity1.energy < 1) { loser = this.entity1.name; }
     if (this.entity2.energy < 1) { loser = this.entity2.name; }
-    return loser;
-}
+    console.log(loser);
+};
 
 module.exports.Do = Do;
-
-
