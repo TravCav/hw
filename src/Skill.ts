eec5c0e36619724fb1cc22d3b00d9c4eecd4206f
cav7ay <jesse.cavanaugh@cchmc.org>
2018-05-25 14:31:37 -0400

    yep

--- src/Skill.ts
+++ src/Skill.ts
@@ -1,78 +1,60 @@
 export interface ISkill {
-    name: string;
-    staminaDrain: number;
-    energyDrain: number;
-    time: number;
-    output: number;
+  name: string;
+  staminaDrain: number;
+  energyDrain: number;
+  time: number;
+  output: number;
 }
 
 export class Skill implements ISkill {
-    public name: string;
-    public staminaDrain: number;
-    public energyDrain: number;
-    public time: number;
-    public output: number;
-    constructor(
-        name: string,
-        staminaDrain: number,
-        energyDrain: number,
-        time: number,
-        output: number) {
-        this.name = name;
-        this.staminaDrain = staminaDrain;
-        this.energyDrain = energyDrain;
-        this.time = time;
-        this.output = output;
-    }
-}
-
-// tslint:disable-next-line:max-classes-per-file
-class Ability implements ISkill {
-    constructor(
-        public name: string,
-        public staminaDrain: number,
-        public energyDrain: number,
-        public time: number,
-        public output: number) { /* stuff */ }
-}
-
-// tslint:disable-next-line:max-classes-per-file
-class Beast {
-    public name: string;
-    public age: number;
-}
-
-// tslint:disable-next-line:max-classes-per-file
-class Creature extends Beast {
-    constructor(public name: string, public age: number) { super(); }
+  public name: string;
+  public staminaDrain: number;
+  public energyDrain: number;
+  public time: number;
+  public output: number;
+  constructor(
+    name: string,
+    staminaDrain: number,
+    energyDrain: number,
+    time: number,
+    output: number
+  ) {
+    this.name = name;
+    this.staminaDrain = staminaDrain;
+    this.energyDrain = energyDrain;
+    this.time = time;
+    this.output = output;
+  }
 }
 
 interface IAnimal {
-    name: string;
-    age: number;
+  name: string;
+  age: number;
 }
 
 // tslint:disable-next-line:max-classes-per-file
 class Animal implements IAnimal {
-    constructor(public name: string, public age: number) { /* constructor */ }
+  constructor(public name: string, public age: number) {
+    /* constructor */
+  }
 }
 
 export const basicSkill: ISkill = {
-    energyDrain: 1,
-    name: "basic skill",
-    output: 1,
-    staminaDrain: 1,
-    time: 1,
+  energyDrain: 1,
+  name: "basic skill",
+  output: 1,
+  staminaDrain: 1,
+  time: 1
 };
 
 const skillz: ISkill[] = [];
 
 const skill1: ISkill = {
-    energyDrain: 1,
-    name: "skill1",
-    output: 1,
-    staminaDrain: 1,
-    time: 1,
+  energyDrain: 1,
+  name: "skill1",
+  output: 1,
+  staminaDrain: 1,
+  time: 1
 };
 skillz.push(skill1);
 
@@ -80,23 +62,23 @@ const skill2: ISkill = new Skill("skill2", 2, 2, 2, 2);
 skillz.push(skill2);
 
 const skill3: Skill = {
-    energyDrain: 3,
-    name: "skill3",
-    output: 3,
-    staminaDrain: 3,
-    time: 3,
+  energyDrain: 3,
+  name: "skill3",
+  output: 3,
+  staminaDrain: 3,
+  time: 3
 };
 skillz.push(skill3);
 
 const skill4: Skill = new Skill("skill4", 4, 4, 4, 4);
 skillz.push(skill4);
 
-const ability1: ISkill = new Ability("ability1", 4, 4, 4, 4);
+const ability1: ISkill = new Skill("ability1", 4, 4, 4, 4);
 skillz.push(ability1);
 
 for (const skill of skillz) {
-    console.log(skill);
-    console.log(typeof (skill as Skill));
-    console.log(skill as Skill instanceof Skill);
-    console.log("");
+  console.log(skill);
+  console.log(typeof (skill as Skill));
+  console.log((skill as Skill) instanceof Skill);
+  console.log("");
 }
